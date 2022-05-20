363b5a2e71d253072bac5d8660608f3ab7a48ffb
sahanivinay <99119816+sahanivinay@users.noreply.github.com>
2022-05-20 17:34:08 +0530

    Add files via upload
    This program prints the grade for marks.

---
+++ ifelse.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int marks;
+    printf("ENTER YOUR MARKS TO GET GRADE: ");
+    scanf("%d",&marks);
+    if(marks<100 && marks>=85){
+        printf("Grade A");
+    }
+    else if(marks<=84 && marks>=70){
+        printf("Grade B");
+    }
+    else if(marks<=69 && marks>=55){
+        printf("Grade C");
+    }
+    else if(marks<=54 && marks>=40){
+        printf("Grade D");
+    }
+    else{
+        printf("Grade F");
+    }
+    return 0;
+}
